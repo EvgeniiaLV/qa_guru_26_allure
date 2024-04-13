044939bb027aad84bde3d57fce50026c24eb1974
Evgeniia Liasheva <153442050+EvgeniiaLV@users.noreply.github.com>
2024-04-14 01:49:37 +0300

    Delete src directory

--- src/test/java/guru/qa/utils/TestData.java
+++
@@ -1,7 +0,0 @@
-package guru.qa.utils;
-
-public class TestData {
-    public static final String REPOSITORY_NAME = "asolntsev/selenide-puzzlers";
-    public static final String WORKFLOW_NUMBER = "#128";
-
-}
